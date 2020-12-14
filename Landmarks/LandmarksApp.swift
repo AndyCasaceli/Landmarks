70057129d00082b94d8c78705c491647f5f60f50
AndyCasaceli <a212881@icloud.com>
2020-12-13 20:30:39 -0500

    Construct landmark detail view.

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Andy Casaceli on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
