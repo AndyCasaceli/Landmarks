f8f47900343bc624fbc2a12870a8f26d49d6b7e2
AndyCasaceli <a212881@icloud.com>
2020-12-14 21:25:38 -0500

    Handle user input section

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Andy Casaceli on 12/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
---
+++ Landmarks/Views/Helpers/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Andy Casaceli on 12/13/20.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    @State private var region = MKCoordinateRegion()
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinate: ModelData().landmarks[0].locationCoordinate)
+    }
+}
